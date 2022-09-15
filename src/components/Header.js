44489d2efeae2a190d9cc89e932f70cf12442e6f
marvo <trujillomarvin@hotmail.com>
2022-09-15 09:32:57 -0500

    bootstrap nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,40 +25,56 @@ const Header = () => {
       </div>
 
       <nav className="navbar navbar-expand-lg">
-        <ul className="navbar-nav align-items-center">
-          <li className="nav-item">
-            <NavLink className="nav-link fs-4" to="/">
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link fs-4" to="/tracker">
-              Tracker
-            </NavLink>
-          </li>
-          {!localAuth.user && (
-            <>
-              <li className="nav-item">
-                <NavLink className="nav-link fs-4" to="/login">
-                  Login
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link fs-4" to="/signup">
-                  Signup
-                </NavLink>
-              </li>
-            </>
-          )}
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarToggler"
+            aria-controls="navbarToggler"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-          {localAuth.user && (
-            <li>
-              <button className="bg-transparent border-0" onClick={logout}>
-                <img width="40px" alt="logout button" src={signoutSVG} />
-              </button>
-            </li>
-          )}
-        </ul>
+          <div className="collapse navbar-collapse" id="navbarToggler">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-center">
+              <li className="nav-item">
+                <NavLink className="nav-link fs-4" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link fs-4" to="/tracker">
+                  Tracker
+                </NavLink>
+              </li>
+              {!localAuth.user && (
+                <>
+                  <li className="nav-item">
+                    <NavLink className="nav-link fs-4" to="/login">
+                      Login
+                    </NavLink>
+                  </li>
+                  <li className=" nav-item">
+                    <NavLink className="nav-link fs-4" to="/signup">
+                      Signup
+                    </NavLink>
+                  </li>
+                </>
+              )}
+
+              {localAuth.user && (
+                <li className="">
+                  <button className="bg-transparent border-0" onClick={logout}>
+                    <img width="40px" alt="logout button" src={signoutSVG} />
+                  </button>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { AuthProvider } from "./components/auth";
 import RouteSwitch from "./RouteSwitch";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.js";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
