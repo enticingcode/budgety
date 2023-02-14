af97983a5513686d4a56771705d60b05720f228e
marvin <trujillomarvin@hotmail.com>
2023-02-14 16:30:11 -0600

    style changes

--- src/components/SavingsModules.js
+++ src/components/SavingsModules.js
@@ -35,7 +35,6 @@ const SavingsModules = (props) => {
     let value = e.target.value;
     let name = e.target.name;
 
-
     if (name === "amount" && isNaN(value)) return;
 
     setSavingsInput((prev) => {
@@ -64,31 +63,15 @@ const SavingsModules = (props) => {
         <p className="money-info">
           {item.name}: ${item.allocation}
         </p>
-        <button onClick={deleteSavings} className="trash-ico">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="white"
-            className="bi bi-trash"
-            viewBox="0 0 16 16"
-          >
-            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
-            <path
-              fillRule="evenodd"
-              d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-            />
-          </svg>
-        </button>
+        <img className="closeOut" src="/xout.png" alt="Delete"></img>
       </div>
     );
   });
 
   return (
     <>
-      <div className="in-headers my-3">
-        <form
-          className="form-inp d-flex align-items-center justify-content-center"
-          onSubmit={addSavingsInput}
-        >
+      <div className="in-headers">
+        <form className="form-inp" onSubmit={addSavingsInput}>
           <input
             onChange={handleChange}
             name="savingsName"
@@ -105,8 +88,8 @@ const SavingsModules = (props) => {
             placeholder="$ Amount"
             required
           ></input>
-          <button type="submit" className="input-btn btn btn-success">
-            Add Allocation
+          <button type="submit" className="input-btn">
+            Add
           </button>
         </form>
         <div className="fin-container">{allocationElements}</div>
